0c5d945076e361685432091164fc2592705170d0
satetheus <personal@cwmccullough.com>
2024-12-08 20:54:50 -0700

    replaced discord with discord-ptb for more updated version

--- configuration.nix
+++ configuration.nix
@@ -121,7 +121,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     brave
-    pkgs.discord
+    pkgs.discord-ptb
     git
     obsidian
 
