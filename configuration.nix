084aeb632ca3dcbe153556bec959912828a0d408
satetheus <personal@cwmccullough.com>
2025-05-05 22:12:02 -0600

    removed a bunch of unneeded comments

--- configuration.nix
+++ configuration.nix
@@ -16,11 +16,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -58,11 +53,6 @@
 
   # configuration for dual monitors
   services.xserver.videoDrivers = [ "nvidia" ];
-  #services.xserver.displayManager.setupCommands = ''
-        #LEFT='HDMI-0'
-        #RIGHT='DP-3'
-        #${pkgs.xorg.xrandr}/bin/xrandr --output $RIGHT --mode 1920x1080 --preferred --output $LEFT --mode 1920x1080 --left-of $RIGHT
-  #'';
 
   hardware.nvidia = {
           modesetting.enable = true;
@@ -70,7 +60,6 @@
           powerManagement.finegrained = false;
           open = true;
           nvidiaSettings = true;
-          #package =  config.boot.kernelPackages.nvidiaPackages.stable;
           package =  config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
@@ -85,16 +74,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.chris = {
@@ -104,7 +84,6 @@
     packages = with pkgs; [
       firefox
       kate
-    #  thunderbird
     ];
   };
 
@@ -121,8 +100,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     brave
     pkgs.discord-ptb
     git
@@ -191,12 +168,10 @@
   };
 
   # steam configuration
-  #programs.steam.enable = true;
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    #localNetworkGameTransfers.openFirewall = true;
   };
 
   # discord configuration
@@ -253,16 +228,6 @@
 
   networking.firewall.enable = true;
   networking.firewall.allowPing = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
