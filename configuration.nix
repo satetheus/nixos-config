d767138a388355292840e3c49cb2343ab1af7f0e
satetheus <personal@cwmccullough.com>
2025-10-30 23:59:22 -0600

    reverted nvidiaPackages back to stable due to issues with drivers.

--- configuration.nix
+++ configuration.nix
@@ -65,7 +65,7 @@
           powerManagement.finegrained = false;
           open = true;
           nvidiaSettings = true;
-          package =  config.boot.kernelPackages.nvidiaPackages.beta;
+          package =  config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # Enable CUPS to print documents.
