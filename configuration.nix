50cbff0f545d59917eadf32141cf38a1de535583
satetheus <personal@cwmccullough.com>
2026-03-14 15:38:11 -0600

    removed smb configuration, not needed now with the nas

--- configuration.nix
+++ configuration.nix
@@ -225,33 +225,6 @@
   # adjust download settings
   nix.settings.download-buffer-size = 500000000; #500MB
 
-  # setup samba for vlc sharing
-  services.samba = {
-        package = pkgs.samba4Full;
-        # ^^ `samba4Full` is compiled with avahi, ldap, AD etc support (compared to the default package, `samba`
-        # Required for samba to register mDNS records for auto discovery
-        # See https://github.com/NixOS/nixpkgs/blob/592047fc9e4f7b74a4dc85d1b9f5243dfe4899e3/pkgs/top-level/all-packages.nix#L27268
-        enable = true;
-        openFirewall = true;
-        shares.testshare = {
-          path = "/mnt/Shares/Public";
-          writable = "true";
-          comment = "Hello World!";
-        };
-  };
-  services.avahi = {
-        publish.enable = true;
-        publish.userServices = true;
-        # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
-        enable = true;
-        openFirewall = true;
-  };
-  services.samba-wsdd = {
-      # This enables autodiscovery on windows since SMB1 (and thus netbios) support was discontinued
-        enable = true;
-        openFirewall = true;
-  };
-
   networking.firewall.enable = true;
   networking.firewall.allowPing = true;
 
